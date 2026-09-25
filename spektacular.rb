00d174c08339999eded5ac6e543a445fe1273d61
Queen Bee <bee@hivecommons.com>
2026-09-25 12:58:51 +0100

    Update version to 0.23.0

--- spektacular.rb
+++ spektacular.rb
@@ -5,26 +5,26 @@
 class Spektacular < Formula
   desc ""
   homepage "https://hivecommons.dev"
-  version "0.22.0"
+  version "0.23.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/hivecommons/spektacular/releases/download/0.22.0/spektacular_0.22.0_darwin_x86_64.zip"
-    sha256 "d51e3705ed8a205cc652265a4d0f5ab0c0799a82dd90ba4e7cc7af298ec468c6"
+    url "https://github.com/hivecommons/spektacular/releases/download/0.23.0/spektacular_0.23.0_darwin_x86_64.zip"
+    sha256 "9005efcb91ebf3a9b19083edc4c1d0f1e4f4b66c994b476ff17abcc45ab3081c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/hivecommons/spektacular/releases/download/0.22.0/spektacular_0.22.0_darwin_arm64.zip"
-    sha256 "81b5c5134c58b907d27670bc2de3f491b2a2cd999c469efcc6ce7130012538dd"
+    url "https://github.com/hivecommons/spektacular/releases/download/0.23.0/spektacular_0.23.0_darwin_arm64.zip"
+    sha256 "76e2fe1181fa71b7e853645a06610e94bdcc9cab3742342113eb7a704aa79664"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/hivecommons/spektacular/releases/download/0.22.0/spektacular_0.22.0_linux_x86_64.tar.gz"
-    sha256 "b244e9f3eae24763ca83d75e642c67aae6b62282c419ca94431a24ffbee65732"
+    url "https://github.com/hivecommons/spektacular/releases/download/0.23.0/spektacular_0.23.0_linux_x86_64.tar.gz"
+    sha256 "208a583b2d9867afd98977e4d120f1a271c131beaaff3c8f8ec9b2b14c8ba465"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/hivecommons/spektacular/releases/download/0.22.0/spektacular_0.22.0_linux_arm64.tar.gz"
-    sha256 "31b5d6816092f7ba86336efa55207aa3a03b367ea699e75594392505c9da6420"
+    url "https://github.com/hivecommons/spektacular/releases/download/0.23.0/spektacular_0.23.0_linux_arm64.tar.gz"
+    sha256 "789a8ea2e34f6b67a11629033d9f0d88ddb5146bdf4200b3e4a1ff90213a6a15"
   end
 
   def install
